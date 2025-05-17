d3edb32156118f1dc3e0130e45e90642eb7533e2
unknown <sajedislam019@gmail.com>
2025-05-18 00:44:38 +0600

    php added

--- form.php
+++ form.php
@@ -13,6 +13,8 @@
     margin-top: 50px;
   }
 </style>
+
+
 </head>
 <body>
   
@@ -22,14 +24,39 @@
       <h2>My First Form php</h2>
       <p>This is a simple PHP application to demonstrate the use of PHP with HTML.</p>
     <h4>out put</h4>
+
     <p>
-      <?php if(isset($_POST['fname']) && !empty($_POST['fname'])):?>
-      <b>First Name : </b><?php echo $_POST['fname'];?>
+
+    <?php
+    $fname = '';
+    $lname = '';
+    if(isset($_REQUEST['fname']) && !empty($_REQUEST['fname'])){
+      // $fname = htmlspecialchars($_REQUEST['fname']);
+      $fname = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_URL);
+      // $fname = filter_input( INPUT_POST, 'fname', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    }
+
+    if(isset($_REQUEST['lname']) && !empty($_REQUEST['lname'])){
+      // $lname = htmlspecialchars($_REQUEST['lname']);
+      $lname = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_URL);
+
+    }
+
+
+      echo "<b>First Name : </b>" . $fname . "<br>";
+      echo "<b>Last Name : </b>" . $lname;
+
+
+    
+    ?>
+      <!-- <?php if(isset($_POST['fname']) && !empty($_POST['fname'])):?>
+      <b>First Name : </b><?php echo htmlspecialchars(  $_POST['fname']);?>
       <?php endif;?>
       <br>
       <?php if(isset($_POST['lname']) && !empty($_POST['lname'])):?>
-      <b>Last Name : </b><?php echo $_POST['lname'];?>
-      <?php endif;?>
+      <b>Last Name : </b><?php echo htmlspecialchars( $_POST['lname']);?>
+      <?php endif;?> -->
     </p>
     
     </div>
@@ -42,9 +69,9 @@
       <p>Fill out the form below and click submit.</p>
       <form  method="POST" action="">
         <label for="fname">First Name</label>
-        <input type="text" placeholder="First Name" name="fname" >
+        <input type="text" placeholder="First Name" name="fname" value ="<?php echo $fname; ?>" >
         <label for="lname">Last Name</label>
-        <input type="text" placeholder="Last Name" name="lname" >
+        <input type="text" placeholder="Last Name" name="lname" value ="<?php echo $lname; ?>" >
         <button type="submit">submit</button>
       </form>
     </div>
